b4288d4b1e1052b515a62ccf7fb6d24e286f433b
yesyesyo_sk <stevekim.0730@gmail.com>
2017-05-24 17:00:39 -0700

    Django

---
+++ python/Django/djenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yesyesyo\desktop\dojo\python\django\djenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python/Django/quick_start/my_project/apps/first_app/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	context = {
+		"greeting": 'hello', 
+		"place": 'world'
+	}
+
+	return render(request, 'first_app/index.html', context)
